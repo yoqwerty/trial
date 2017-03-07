f432524a9048c95532e91a73310eb971569228a6
yoqwerty <ssupreetkaur420@gmail.com>
2017-03-08 00:18:46 +0530

    Add files via upload

---
+++ JavaApplication1/src/NewClass.java
@@ -0,0 +1,25 @@
+
+import java.util.Vector;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author supreet
+ */
+public class NewClass {
+    public void hi(){
+         v =new Vector();
+        v.add(1);
+    }
+   public static void main(String[] args){
+       
+      NewClass12 y = new NewClass12();
+      y.fun();
+    }
+    public Vector v ;
+}
---
+++ JavaApplication1/src/NewClass12.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author supreet
+ */
+public class NewClass12 {
+    void fun(){
+        NewClass n =new NewClass();
+        n.hi();
+        System.out.println(n.v.elementAt(0));
+    }
+}
